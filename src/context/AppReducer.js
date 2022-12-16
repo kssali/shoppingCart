600d807fc537fcb534c2b3605add33d8dc712ea6
saliks <kssalianu1990@gmail.com>
2022-12-16 11:59:05 +0530

    first commit added

---
+++ src/context/AppReducer.js
@@ -0,0 +1,53 @@
+import { productData } from "../data"
+
+
+export const initialState = {
+    siteName:"React Shop",
+    cart:[],
+    products:productData,
+    products_loading: false,
+}
+
+export const reducer = (prevState,action) => {
+    switch(action.type){
+
+        // Fetched products from server
+        case "PRODUCTS_LOADED":
+            return {...prevState, products:[...action.payload], products_loading:true} 
+
+        // ADD Product to Cart
+        case "ADD_TO_CART": 
+            return {...prevState, cart:[...prevState.cart,action.payload] }   
+
+        // Increase Product Count 
+        case "INCREASE_PRODUCT_COUNT":
+            let incrementedCart = prevState.cart.map(item => {
+                if(item.id === action.payload){
+                    return {...item, count: item.count + 1}
+                }
+                return item
+            })
+            return {...prevState, cart:[...incrementedCart]} 
+
+        // Decrease Product Count 
+        case "DECREASE_PRODUCT_COUNT":
+            let decrementedCart = prevState.cart.map(item => {
+                if(item.id === action.payload){
+                    return {...item, count: item.count - 1}
+                }
+                return item
+            })
+            return {...prevState, cart:[...decrementedCart]}  
+
+        // clearing cart
+        case "CLEAR_CART":
+            return {...prevState, cart:[]}
+
+        // removing a product
+        case "REMOVE_PRODUCT":
+            return {...prevState, cart: prevState.cart.filter(item => item.id !== action.payload)}
+
+        default:
+            return prevState    
+    }
+}
